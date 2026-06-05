01f497c3679aa5306ba684c0d1a40a114c7ae86e
mateusdiasyt <mateusdiasyt@users.noreply.github.com>
2026-06-05 09:56:13 -0300

    chore(accounts): rename add account button

--- src/presentation/admin/accounts/accounts-spreadsheet.tsx
+++ src/presentation/admin/accounts/accounts-spreadsheet.tsx
@@ -275,7 +275,7 @@ export function AccountsSpreadsheet({ accounts }: AccountsSpreadsheetProps) {
 
             <Button type="button" size="sm" className="gap-2" onClick={() => setFormOpen(true)}>
               <Plus className="h-4 w-4" />
-              Adicionar Conta Fixa
+              Adicionar Conta
             </Button>
           </div>
         </div>
